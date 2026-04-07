5707647ee7618a8fad405124fcc63b3cbb944515
Fayerblue1 <affanthoriq120@gmail.com>
2026-04-07 16:26:41 +0800

    Actions & Method

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -10,6 +10,19 @@ class Dashboard extends Component
     public $aset = 1500000;
     public $transactioncount = 0;
 
+
+    public function plusAssets()
+    {
+        $this->aset += 500000; //Tambah aset sebesar 500.000
+        $this->transactioncount++; //Tambah jumlah transaksi sebanyak 1
+    }
+    public function minusAssets()
+    {
+       if ($this->aset >= 500000) { // Pastikan aset tidak menjadi negatif
+            $this->aset -= 500000; // Kurangi aset sebesar 500.000
+            $this->transactioncount++; // Tambah jumlah transaksi sebanyak 1
+        }
+    }
     public function render()
     {
         return view('livewire.dashboard');
